a9d515c772be2e61094ac049dab0cffcac7ed48a
nikhomgithub <48146261+nikhomgithub@users.noreply.github.com>
2019-09-24 11:29:04 +0700

    Update node_sql_brad.js

--- node_sql_brad.js
+++ node_sql_brad.js
@@ -1,3 +1,4 @@
+/*
 install XAMPP 
      for Apache server, PHP MyAdmin (For my sql dashboard)
 
@@ -9,10 +10,55 @@ $npm install -g nodemon
 
 --------
 --------
+
 app.js
+*/
 
 const express = require('express');
 const mysql = require('mysql');
+
+//Create connection
+const db = mysql.createConnection({
+     host:'localhost',
+     user:'root',
+     password:'123456',
+     database:'nodemysql' //No need in case to need create a database with node
+});
+
+//connect
+db.connect((err)=>{
+    if(err){
+          throw err;
+    }
+    cosole.log('MySql Connected....');
+});
+
+//Create DB just once and need to cancel //database:'nodemysql'
+app.get('/createdb',(req,res)=>{
+     let sql = 'CREATE DATABASE nodemysql'
+     db.query(sql,(err,result)=>{
+          if(err){
+               throw err
+          }
+          console.log(result);
+          res.send('Database created...');
+     });
+});
+
+//Create a table 
+app.get('/createposttable',(req,res)=>{
+     let sql = 'CREATE TABEL posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY(id))';
+     db.query(sql,(err,result)=>{
+          if(err){
+               throw err;
+          }
+          console.log(result);
+          res.send('posts table created...');
+     })
+     
+     
+});, 
+
 
 const app = express();
 
